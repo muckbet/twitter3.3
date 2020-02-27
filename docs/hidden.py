b4c0af3b251bf590458623373b9a729f41c42602
Hryhoriy Ovsiannikov <ovsiannikov@ucu.edu.ua>
2020-02-27 15:24:06 +0200

    init

---
+++ docs/hidden.py
@@ -0,0 +1,11 @@
+# Keep this file separate
+# Data of API-key
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "key",
+            "consumer_secret": "key",
+            "token_key": "key",
+            "token_secret": "key"}
